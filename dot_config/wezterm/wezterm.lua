8af6929d45c5b2af1b5a8bce1584d1395d8a36e4
Rene Cortez <reco@pop-os.localdomain>
2023-09-11 10:46:13 -0500

    Initial commit

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,77 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+return {
+  default_prog = { '/usr/bin/bash' },
+  window_decorations = "RESIZE",
+  color_scheme = "nordfox",
+  font = wezterm.font_with_fallback({
+    "JetBrains Mono",
+    { family = "Symbols Nerd Font Mono", scale = 0.75 }
+  }),
+  use_cap_height_to_scale_fallback_fonts = true,
+  font_size = 13,
+  scrollback_lines = 5000,
+  hide_tab_bar_if_only_one_tab = true,
+  audible_bell = "Disabled",
+  exit_behavior = "Close",
+  window_background_opacity = 0.85,
+  keys = {
+    {
+      key = 'l',
+      mods = 'ALT',
+      action = wezterm.action.SplitPane {
+        direction = 'Right',
+        size = { Percent = 50 },
+      },
+    },
+    {
+      key = 'j',
+      mods = 'ALT',
+      action = wezterm.action.SplitPane {
+        direction = 'Down',
+        size = { Percent = 50 },
+      },
+    },
+    {
+      key = 'h',
+      mods = 'ALT',
+      action = wezterm.action.SplitPane {
+        direction = 'Left',
+        size = { Percent = 50 },
+      },
+    },
+    {
+      key = 'k',
+      mods = 'ALT',
+      action = wezterm.action.SplitPane {
+        direction = 'Up',
+        size = { Percent = 50 },
+      },
+    },
+    {
+      key = 'w',
+      mods = 'ALT',
+      action = wezterm.action.CloseCurrentPane { confirm = false },
+    },
+    {
+      key = 'l',
+      mods = 'CTRL',
+      action = wezterm.action.ActivatePaneDirection 'Next',
+    },
+    {
+      key = 'l',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.ActivatePaneDirection 'Prev',
+    },
+  },
+}
